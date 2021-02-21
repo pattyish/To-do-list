f6af83d10938df8ab8c129c60ee03add49e694f6
pattyish <patrickishimwe10@gmail.com>
2021-02-21 12:55:21 +0200

    simple style and different way of style application

--- Components/TodoList.js
+++ Components/TodoList.js
@@ -1,18 +1,28 @@
 import React, { useState } from 'react';
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, StyleSheet } from 'react-native'
 import Todo from './Todo';
 
 const TodoLIst = () => {
     const [title, setTitle] = useState("Todo List")
 
     return (
-        <View>
-            <Text>{title}</Text>
+        <View style={{ width: '80%', marginBottom: 60 }}>
+            <Text style={[styles.align, styles.font]}>{title}</Text>
             <Todo name={'First Todo'} />
             <Todo name={'Second Todo'} />
             <Button title="Change Me" onPress={() => setTitle("My List")} />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    align: {
+        alignSelf: "center",
+    },
+    font: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
+})
 
 export default TodoLIst;
