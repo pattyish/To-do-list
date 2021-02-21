691d1c1658e64dcb566684d5eb524aad58fe54e9
pattyish <patrickishimwe10@gmail.com>
2021-02-21 13:49:45 +0200

    add todo to the list

--- Components/Todo.js
+++ Components/Todo.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native'
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
 
 const Todo = (props) => {
-    return (
-        <View style={styles.item}>
-            <Text>{props.name}</Text>
-        </View>
-    );
-}
+  return (
+    <View key={props.key} style={[styles.item, { padding: 8 }]}>
+      <Text>{props.todo}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    item: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        borderColor: 'grey',
-        borderWidth: 1,
-        borderRadius: 5,
-        backgroundColor: 'whitesmoke',
-        marginBottom: 2
-    }
-})
+  item: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderColor: "grey",
+    borderWidth: 1,
+    borderRadius: 5,
+    backgroundColor: "whitesmoke",
+    marginBottom: 2,
+  },
+});
 
 export default Todo;
--- Components/TodoList.js
+++ Components/TodoList.js
@@ -1,44 +1,60 @@
-import React, { useState } from 'react';
-import { View, Text, Button, StyleSheet, ScrollView, TextInput } from 'react-native'
-import Todo from './Todo';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+} from "react-native";
+import Todo from "./Todo";
 
 const TodoLIst = () => {
-    const [title, setTitle] = useState("Todo List")
+  const [title, setTitle] = useState("Todo List");
+  const [text, setText] = useState();
+  const [list, setList] = useState(["hello world!!"]);
 
-    return (
-        <View style={{ width: '80%', marginBottom: 60 }}>
-            <Text style={[styles.align, styles.font]}>{title}</Text>
-            <ScrollView>
-                <Todo name={'First Todo'} />
-                <Todo name={'Second Todo'} />
-                <Todo name={'Three Todo'} />
-                <Todo name={'Four Todo'} />
-                <Todo name={'Five Todo'} />
-                <Todo name={'Sex Todo'} />
-            </ScrollView>
-            <View>
-                <TextInput style={styles.input} />
-                <Button title="Add Item" onPress={() => setTitle("My List")} />
-            </View>
-        </View>
-    )
-}
+  const AddItem = () => {
+    const updatedList = list;
+    updatedList.push(text);
+    setList(updatedList);
+    setText(" ");
+  };
+  return (
+    <View style={{ width: "80%", marginBottom: 60 }}>
+      <Text style={[styles.align, styles.font]}>{title}</Text>
+      <ScrollView>
+        {list.map((todo, index) => (
+          <Todo key={index} todo={todo} />
+        ))}
+      </ScrollView>
+      <View>
+        <TextInput
+          style={styles.input}
+          value={text}
+          onChangeText={(text) => setText(text)}
+        />
+        <Button title="Add Item" onPress={AddItem} />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    align: {
-        alignSelf: "center",
-    },
-    font: {
-        fontSize: 20,
-        fontWeight: 'bold'
-    },
-    input: {
-        borderRadius: 5,
-        borderWidth: 1,
-        marginBottom: 8,
-        marginTop: 8,
-        padding: 5
-    }
-})
+  align: {
+    alignSelf: "center",
+  },
+  font: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  input: {
+    borderRadius: 5,
+    borderWidth: 1,
+    marginBottom: 8,
+    marginTop: 8,
+    padding: 5,
+  },
+});
 
 export default TodoLIst;
